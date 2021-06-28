9c259c08090bee09872554bcf72b7e8ab4741dd3
moroshandmitry <moroshandmitry@gmail.com>
2021-06-28 14:08:15 +0300

    created structure app

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        font-size: 62.5%; // 10px => 1rem
+        height: 100%;
+    }
+
+    body {
+        height: 100%;
+    }
+
+    body,
+    input,
+    button,
+    textarea {
+        font-size: ${props => props.theme.defaultFontSize};
+        font-weight: 500;
+        font-family: ${props => props.theme.defaultFontFamily};
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    #root {
+        height: 100%;
+    }
+`;
