cc7ea7b33da04ed0800228b6ad77538c76f06d75
moroshandmitry <moroshandmitry@gmail.com>
2021-06-29 00:07:52 +0300

    first fake path config of the router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 // import { useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
 
 import { Layout } from './components';
 import { LayoutContainer } from './containers';
+import { HomePage } from './pages';
 import { GlobalStyles } from './styles/GlobalStyles';
 import {
     darkTheme
     // lightTheme
 } from './themes';
+
+const FakePage = props => {
+    console.log('[props]', props);
+    return <p>I'm a fake page!</p>;
+};
 
 export const App = () => {
     // const [theme, setTheme] = useState(darkTheme);
@@ -19,24 +26,23 @@ export const App = () => {
     //     });
 
     return (
-        <ThemeProvider theme={darkTheme}>
-            {/* <span>Theme</span>{' '}
+        <BrowserRouter>
+            <ThemeProvider theme={darkTheme}>
+                {/* <span>Theme</span>{' '}
             <button type="button" onClick={handleSwitchTheme}>
                 Switch to Light Theme
             </button> */}
-            <GlobalStyles />
-            <LayoutContainer>
-                {({ movies, ...otherProps }) => (
-                    <Layout {...otherProps}>
-                        {console.log('[otherProps]', otherProps)}
-                        {console.log('[movies]', movies)}
-                        <span>Movies list:</span>
-                        {movies.map(movie => (
-                            <div key={movie.id}>Movie ID: {movie.id}</div>
-                        ))}
-                    </Layout>
-                )}
-            </LayoutContainer>
-        </ThemeProvider>
+                <GlobalStyles />
+                <LayoutContainer>
+                    {({ movies, ...otherProps }) => (
+                        <Layout {...otherProps}>
+                            <Route path="/" component={FakePage} />
+
+                            <HomePage movies={movies} />
+                        </Layout>
+                    )}
+                </LayoutContainer>
+            </ThemeProvider>
+        </BrowserRouter>
     );
 };
