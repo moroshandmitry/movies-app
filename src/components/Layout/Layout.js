c3ac93ccff93544d2089b4b2855b1d5e0f907d88
moroshandmitry <moroshandmitry@gmail.com>
2021-06-28 17:21:12 +0300

    added components Button, Input for Header

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,25 @@
+import PT from 'prop-types';
+
 import { Header } from '../Header';
 import { Footer } from '../Footer';
+import { StyledWidthLimiter } from '../../styles';
 import { StyledLayout, StyledMain } from './styles';
 
 export const Layout = ({ children }) => (
     <StyledLayout>
         <Header />
-        <StyledMain>{children}</StyledMain>
+
+        <StyledMain>
+            <StyledWidthLimiter>{children}</StyledWidthLimiter>
+        </StyledMain>
+
         <Footer />
     </StyledLayout>
 );
+
+Layout.propTypes = {
+    /**
+     * Page representation (Component to render)
+     */
+    children: PT.node.isRequired
+};
--- src/themes/dark/theme.js
+++ src/themes/dark/theme.js
@@ -1,8 +1,17 @@
 import * as commonVariables from '../variables';
 
 export const theme = {
-    defaultFontFamily: commonVariables.fontFamily,
-    defaultFontSize: commonVariables.fontSize.md,
-    bgColor: 'darkblue',
-    color: '#fff'
+    ...commonVariables,
+    header: {
+        background: 'linear-gradient(to right, #2a0845, #6441A5)',
+        color: '#fff'
+    },
+    footer: {
+        background: 'linear-gradient(to right, #2a0845, #6441A5)',
+        color: '#fff'
+    },
+    button: {
+        background: commonVariables.lightColors[500],
+        color: commonVariables.darkColors[900]
+    }
 };
