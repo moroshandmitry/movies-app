55f67e5d9b631f08648474975b068b8b6876c48b
moroshandmitry <moroshandmitry@gmail.com>
2021-07-06 11:08:00 +0300

    StubComponent for HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,23 +5,34 @@ import { StyledWrapper, StyledCard } from './styles';
 
 const { REACT_APP_STORAGE_IMAGES_URL } = process.env;
 
+const StubComponent = () => (
+    <h1 style={{ color: 'white' }}>
+        Please press the button for searching lucky movies, or write you movie
+        in input and press button search.
+    </h1>
+);
+
 export const HomePage = ({ movies }) => (
     <StyledWrapper>
-        {movies
-            .filter(({ poster_path }) => poster_path !== null)
-            .map(({ id, original_title, poster_path }) => {
-                const imageUrl =
-                    REACT_APP_STORAGE_IMAGES_URL +
-                    (poster_path !== null
-                        ? poster_path
-                        : 'tzve3LD534wsCnhOrSqgJ1mnRma.jpg');
+        {movies.length !== 0 ? (
+            movies
+                .filter(({ poster_path }) => poster_path !== null)
+                .map(({ id, original_title, poster_path }) => {
+                    const imageUrl =
+                        REACT_APP_STORAGE_IMAGES_URL +
+                        (poster_path !== null
+                            ? poster_path
+                            : 'tzve3LD534wsCnhOrSqgJ1mnRma.jpg');
 
-                return (
-                    <StyledCard key={id} $imageUrl={imageUrl}>
-                        <Link to={`/movie/${id}`}>{original_title}</Link>
-                    </StyledCard>
-                );
-            })}
+                    return (
+                        <StyledCard key={id} $imageUrl={imageUrl}>
+                            <Link to={`/movie/${id}`}>{original_title}</Link>
+                        </StyledCard>
+                    );
+                })
+        ) : (
+            <StubComponent />
+        )}
     </StyledWrapper>
 );
 
